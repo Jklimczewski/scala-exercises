2ede243863094b6e5e768d52f503d3fe45dca61e
Jakub Klimczewski <smurf_2002@o2.pl>
2022-03-21 14:16:38 +0100

    poprawki

--- lab01/lab01zad2.scala
+++ lab01/lab01zad2.scala
@@ -1,4 +1,4 @@
-@main def obramuj(napis: String): Unit = {
+@main def lab01zad2(napis: String): Unit = {
     println("*"*(napis.length+2))
     println("*" + napis + "*")
     println("*"*(napis.length+2))
--- lab02/lab02zad4.scala
+++ lab02/lab02zad4.scala
@@ -3,7 +3,7 @@ def hipoteza(n: Int, x: Int = 2): Unit = {
     else {
         if (pierwsza(x) == true) {
             if (pierwsza(n-x) == true) {
-                println(n, " = " , x , " + " , (n-x))
+                println(n + " = " + x + " + " + (n-x))
             }
             else hipoteza(n, x=x+1)
         }
@@ -21,4 +21,4 @@ def pierwsza(x: Int, y: Int = 2): Boolean = {
 @main def lab2zad4(n: Int): Unit = {
     if (n % 2 == 0 && n > 2) hipoteza(n)
     else println("Podales zla liczbe")
-}
+}	
