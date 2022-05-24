694d18f74a39d5cee4dae9efaae395016b836c7a
Jakub Klimczewski <smurf_2002@o2.pl>
2022-05-24 21:06:25 +0200

    poprawione lab3zad3

--- lab03/lab03zad3.scala
+++ lab03/lab03zad3.scala
@@ -1,14 +1,15 @@
 def ciag(n: Int): Int = {
-    // @annotation.tailrec
-    def helper(n: Int, wart: Int = 0): Int = {
-        if (n == 2) wart
-        else if (n == 2) wart=wart+3
-        else helper(n-1) + helper(n-2)
+    def helper(n: Int, prev: Int = 1, wynik: Int = 3, licznik: Int = 3): Int = {
+        if (n == licznik) wynik
+        else helper(n, wynik, wynik + prev, licznik + 1)
     }
-    if (n == 0) 2
-    else if (n == 1) 1
+    if (n == 1) 2
+    else if (n == 2) 1
     else helper(n)
 }
+
+
+
 @main def lab3zad3(): Unit = {
     println(ciag(9))
 }
